401b3bda366dd6baf709d21a8ad4241251b63056
Hristo Gatsinski <gatsinski@gmail.com>
2019-01-05 14:01:30 +0200

    Add `jquery`

--- contuga/settings.py
+++ contuga/settings.py
@@ -45,6 +45,7 @@ PIPELINE = {
     'JAVASCRIPT': {
         'main': {
             'source_filenames': (
+                'jquery/dist/jquery.js',
                 'bootstrap/dist/js/bootstrap.bundle.js',
                 'js/main.js',
             ),
