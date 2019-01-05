639393b560d097125ff61d671fb3fd31d26dba60
Hristo Gatsinski <gatsinski@gmail.com>
2019-01-05 13:53:24 +0200

    Add `node_modules` to `STATICFILES_DIRS`

--- contuga/settings.py
+++ contuga/settings.py
@@ -20,6 +20,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'node_modules'),
+)
+
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
