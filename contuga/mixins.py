ba8747d29e3b76ebed3cc49d78a909c5f6700c0d
Hristo Gatsinski <gatsinski@gmail.com>
2019-04-17 21:39:54 +0300

    Add `accounts` app

--- contuga/mixins.py
+++ contuga/mixins.py
@@ -2,3 +2,9 @@ class OnlyAuthoredByCurrentUserMixin:
 
     def get_queryset(self):
         return super().get_queryset().filter(author=self.request.user)
+
+
+class OnlyOwnedByCurrentUserMixin:
+
+    def get_queryset(self):
+        return super().get_queryset().filter(owner=self.request.user)
