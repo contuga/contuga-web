683e498659c91d4b53579f4b255dc71ca9dce551
Hristo Gatsinski <gatsinski@gmail.com>
2019-11-17 11:49:07 +0200

    Split settings into components and environments

---
+++ contuga/settings/components/internationalization.py
@@ -0,0 +1,22 @@
+import os
+
+PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.11/topics/i18n/
+
+LANGUAGE_CODE = "en"
+
+LANGUAGES = [("en", "English"), ("bg", "Български")]
+
+LOCALE_PATHS = (os.path.join(PROJECT_PATH, "locale/"),)
+
+USE_I18N = True
+
+USE_L10N = True
+
+# Formatting
+# https://docs.djangoproject.com/en/1.11/topics/i18n/formatting/
+FORMAT_MODULE_PATH = ["contuga.formats"]
+USE_THOUSAND_SEPARATOR = True
+NUMBER_GROUPING = 3
