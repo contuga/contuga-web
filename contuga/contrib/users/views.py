156f776c83c705eb0f7897d49abd0c7c4c742c81
Hristo Gatsinski <gatsinski@gmail.com>
2019-11-24 13:08:56 +0200

    Add password reset and password change options

--- contuga/contrib/users/views.py
+++ contuga/contrib/users/views.py
@@ -57,6 +57,35 @@ class LogoutView(auth_views.LogoutView):
     pass
 
 
+class PasswordChangeView(auth_views.PasswordChangeView):
+    template_name = "users/password_change_form.html"
+    success_url = reverse_lazy("users:password_change_done")
+
+
+class PasswordChangeDoneView(auth_views.PasswordChangeDoneView):
+    template_name = "users/password_change_done.html"
+
+
+class PasswordResetView(auth_views.PasswordResetView):
+    template_name = "users/password_reset_form.html"
+    email_template_name = "users/password_reset_email.html"
+    subject_template_name = "users/password_reset_subject.txt"
+    success_url = reverse_lazy("users:password_reset_done")
+
+
+class PasswordResetDoneView(auth_views.PasswordResetDoneView):
+    template_name = "users/password_reset_done.html"
+
+
+class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+    template_name = "users/password_reset_confirm.html"
+    success_url = reverse_lazy("users:password_reset_complete")
+
+
+class PasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+    template_name = "users/password_reset_complete.html"
+
+
 class UserDetailView(mixins.LoginRequiredMixin, generic.DetailView):
     model = UserModel
 
