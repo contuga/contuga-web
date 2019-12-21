c9313f10c54c4a411a83e5fd211f6d9630540a88
Hristo Gatsinski <gatsinski@gmail.com>
2019-12-21 19:18:00 +0200

    Add backend transaction filtering, sorting and searching

---
+++ contuga/views.py
@@ -0,0 +1,17 @@
+from django.views import generic
+
+
+class FilteredListView(generic.ListView):
+    filterset_class = None
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = self.filterset_class(
+            self.request.GET, request=self.request, queryset=queryset
+        )
+        return self.filterset.qs.distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["filterset"] = self.filterset
+        return context
