4fec4d75d984e3b1beb96f829591ea23c9cf1fe7
Hristo Gatsinski <gatsinski@gmail.com>
2019-12-24 15:39:13 +0200

    Add TransactionResource

---
+++ contuga/contrib/transactions/resources.py
@@ -0,0 +1,14 @@
+from import_export import resources
+from .models import Transaction
+
+
+class TransactionResource(resources.ModelResource):
+    class Meta:
+        model = Transaction
+        fields = ("type", "amount", "category", "account", "description")
+
+    def dehydrate_category(self, transaction):
+        return transaction.category.name if transaction.category else ""
+
+    def dehydrate_account(self, transaction):
+        return transaction.account.name
