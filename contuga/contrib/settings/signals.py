d25f6c25e85c44301533ed4b5a02e0437b62c168
Hristo Gatsinski <gatsinski@gmail.com>
2020-02-02 22:01:39 +0200

    Add settings app

---
+++ contuga/contrib/settings/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+
+from .models import Settings
+
+UserModel = get_user_model()
+
+
+@receiver(post_save, sender=UserModel, dispatch_uid="create_user_settings")
+def create_user_settings(sender, instance, created, **kwargs):
+    if created:
+        Settings.objects.create(user=instance)
---
+++ contuga/contrib/settings/tests.py
@@ -0,0 +1,90 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+from contuga.contrib.categories.models import Category
+from contuga.contrib.accounts.models import Account
+from contuga.contrib.accounts.constants import BGN
+
+from .models import Settings
+
+UserModel = get_user_model()
+
+
+class SettingsSignalsTests(TestCase):
+    def test_signals(self):
+        old_settings_count = Settings.objects.count()
+        user = UserModel.objects.create_user("john.doe@example.com", "password")
+        new_settings_count = Settings.objects.count()
+
+        # Assert new settings instance is created
+        self.assertEqual(new_settings_count, old_settings_count + 1)
+
+        # Assert settings instance belongs to the correct user
+        settings = Settings.objects.last()
+        self.assertEqual(settings.user, user)
+
+
+class SettingsViewTests(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create_user("john.doe@example.com", "password")
+        self.settings = Settings.objects.last()
+        self.client.force_login(self.user)
+
+    def test_detail(self):
+        url = reverse("settings:detail")
+        response = self.client.get(url, follow=True)
+
+        # Assert status code is correct
+        self.assertEqual(response.status_code, 200)
+
+        # Assert correct model is retrieved
+        self.assertEqual(response.context["settings"], self.settings)
+
+    def test_update(self):
+        category = Category.objects.create(
+            name="Second category name",
+            author=self.user,
+            description="Second category description",
+        )
+        account = Account.objects.create(
+            name="Second account name",
+            currency=BGN,
+            owner=self.user,
+            description="Second account description",
+        )
+        data = {"default_category": category.pk, "default_account": account.pk}
+
+        url = reverse("settings:update")
+        response = self.client.post(url, data=data, follow=True)
+
+        # Assert user is redirected to detail view
+        self.assertRedirects(
+            response,
+            reverse("settings:detail"),
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True,
+        )
+
+        # Assert settings are updated
+        updated_settings = Settings.objects.get(pk=self.settings.pk)
+        settings_data = {
+            "pk": updated_settings.pk,
+            "default_category": updated_settings.default_category,
+            "default_account": updated_settings.default_account,
+        }
+        expected_data = {
+            "pk": self.user.pk,
+            "default_category": category,
+            "default_account": account,
+        }
+        self.assertDictEqual(settings_data, expected_data)
+
+    def test_delete(self):
+        old_settings_count = Settings.objects.count()
+        self.user.delete()
+        new_settings_count = Settings.objects.count()
+
+        # Assert settings are deleted
+        self.assertEqual(new_settings_count, old_settings_count - 1)
