28d0543239f58f2f7a76e0b834c41d5c7f912942
Hristo Gatsinski <gatsinski@gmail.com>
2020-02-09 00:00:54 +0200

    Add default categories

---
+++ contuga/contrib/categories/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+from .models import Category
+
+UserModel = get_user_model()
+
+
+@receiver(post_save, sender=UserModel, dispatch_uid="create_user_categories")
+def create_user_categories(sender, instance, created, **kwargs):
+
+    if created:
+        for category in settings.DEFAULT_CATEGORIES:
+            Category.objects.create(name=category, author=instance)
