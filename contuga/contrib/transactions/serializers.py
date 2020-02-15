5b85a1ef2857f7b1534abcaddf1ca331c1c14ce6
Hristo Gatsinski <gatsinski@gmail.com>
2020-02-15 15:28:27 +0200

    Add Transactions API

---
+++ contuga/contrib/transactions/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Transaction
+
+
+class TransactionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = "__all__"
+        extra_kwargs = {"author": {"read_only": True}}
