f6f9bf113ef5c5d61736e95a8addcf7accbf95f1
Hristo Gatsinski <gatsinski@gmail.com>
2020-02-15 12:31:51 +0200

    Add Accounts API

---
+++ contuga/contrib/accounts/tests/test_api_list.py
@@ -0,0 +1,131 @@
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+from rest_framework.test import APITestCase
+
+from ..models import Account
+from .. import constants
+from . import utils
+
+UserModel = get_user_model()
+
+
+class AccountListTestCase(APITestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create_user("john.doe@example.com", "password")
+        self.account = Account.objects.create(
+            name="Account name",
+            currency=constants.BGN,
+            owner=self.user,
+            description="Account description",
+        )
+        self.client.force_login(self.user)
+
+    def test_get(self):
+        url = reverse("account-list")
+
+        # Creating another user and account to make sure the currently
+        # logged in user cannot see the accounts of other users
+        utils.create_user_and_account()
+
+        response = self.client.get(url, format="json")
+
+        # Assert status code is correct
+        self.assertEqual(response.status_code, 200)
+
+        # Assert correct data is returned
+        expected_response = {
+            "count": 1,
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "url": response.wsgi_request.build_absolute_uri(
+                        reverse("account-detail", args=[self.account.pk])
+                    ),
+                    "name": self.account.name,
+                    "currency": self.account.currency,
+                    "owner": response.wsgi_request.build_absolute_uri(
+                        reverse("user-detail", args=[self.user.pk])
+                    ),
+                    "description": self.account.description,
+                    "is_active": self.account.is_active,
+                    "updated_at": self.account.updated_at.astimezone().isoformat(),
+                    "created_at": self.account.created_at.astimezone().isoformat(),
+                }
+            ],
+        }
+
+        self.assertDictEqual(response.json(), expected_response)
+
+    def test_post(self):
+        url = reverse("account-list")
+
+        data = {
+            "name": "New account name",
+            "currency": constants.EUR,
+            "description": "New account description",
+            "is_active": False,
+        }
+
+        response = self.client.post(url, data=data, format="json")
+
+        # Assert status code is correct
+        self.assertEqual(response.status_code, 201)
+
+        # Assert correct data is returned
+        account = Account.objects.order_by("pk").last()
+
+        expected_response = {
+            "url": response.wsgi_request.build_absolute_uri(
+                reverse("account-detail", args=[account.pk])
+            ),
+            "name": account.name,
+            "currency": account.currency,
+            "owner": response.wsgi_request.build_absolute_uri(
+                reverse("user-detail", args=[self.user.pk])
+            ),
+            "description": account.description,
+            "is_active": account.is_active,
+            "updated_at": account.updated_at.astimezone().isoformat(),
+            "created_at": account.created_at.astimezone().isoformat(),
+        }
+
+        self.assertDictEqual(response.json(), expected_response)
+
+    def test_owner_field_is_ignored_on_post(self):
+        url = reverse("account-list")
+        user = UserModel.objects.create_user("richard.roe@example.com", "password")
+
+        data = {
+            "name": "New account name",
+            "currency": constants.EUR,
+            "owner": reverse("user-detail", args=[user.pk]),
+            "description": "New account description",
+            "is_active": False,
+        }
+
+        response = self.client.post(url, data=data, format="json")
+
+        # Assert status code is correct
+        self.assertEqual(response.status_code, 201)
+
+        # Assert correct data is returned
+        account = Account.objects.order_by("pk").last()
+
+        expected_response = {
+            "url": response.wsgi_request.build_absolute_uri(
+                reverse("account-detail", args=[account.pk])
+            ),
+            "name": account.name,
+            "currency": account.currency,
+            "owner": response.wsgi_request.build_absolute_uri(
+                reverse("user-detail", args=[self.user.pk])
+            ),
+            "description": account.description,
+            "is_active": account.is_active,
+            "updated_at": account.updated_at.astimezone().isoformat(),
+            "created_at": account.created_at.astimezone().isoformat(),
+        }
+
+        self.assertDictEqual(response.json(), expected_response)
