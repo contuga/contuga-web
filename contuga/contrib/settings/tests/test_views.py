eab329a175064f299c0e0cf01e6b2ecbf6c10c83
Hristo Gatsinski <gatsinski@gmail.com>
2020-02-15 12:30:43 +0200

    Add Settings API

--- contuga/contrib/settings/tests/test_views.py
+++ contuga/contrib/settings/tests/test_views.py
@@ -6,23 +6,9 @@ from contuga.contrib.categories.models import Category
 from contuga.contrib.accounts.models import Account
 from contuga.contrib.accounts.constants import BGN
 
-from .models import Settings
+from ..models import Settings
 
 UserModel = get_user_model()
-
-
-class SettingsSignalsTests(TestCase):
-    def test_signals(self):
-        old_settings_count = Settings.objects.count()
-        user = UserModel.objects.create_user("john.doe@example.com", "password")
-        new_settings_count = Settings.objects.count()
-
-        # Assert new settings instance is created
-        self.assertEqual(new_settings_count, old_settings_count + 1)
-
-        # Assert settings instance belongs to the correct user
-        settings = Settings.objects.last()
-        self.assertEqual(settings.user, user)
 
 
 class SettingsViewTests(TestCase):
