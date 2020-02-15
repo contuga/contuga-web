11a84acc46bc96c9151c10b6800864589db3fddb
Hristo Gatsinski <gatsinski@gmail.com>
2020-02-15 12:29:41 +0200

    Add Users API

---
+++ contuga/contrib/users/serializers.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
+
+from rest_framework import serializers
+
+UserModel = get_user_model()
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ("url", "email", "password", "last_login", "date_joined")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+
+class UserUpdateSerializer(serializers.HyperlinkedModelSerializer):
+    current_password = serializers.CharField(max_length=128, write_only=True)
+    new_password = serializers.CharField(max_length=128, write_only=True)
+
+    class Meta:
+        model = UserModel
+        fields = (
+            "url",
+            "email",
+            "last_login",
+            "date_joined",
+            "current_password",
+            "new_password",
+        )
+        extra_kwargs = {"email": {"read_only": True}}
+
+    def update(self, instance, validated_data):
+        new_password = validated_data.get("new_password")
+        instance.set_password(new_password)
+        instance.save()
+        return instance
+
+    def validate_current_password(self, value):
+        if not self.instance.check_password(value):
+            message = _("Invalid current password")
+            raise serializers.ValidationError({"current_password": message})
+        return value
--- contuga/contrib/users/views.py
+++ contuga/contrib/users/views.py
@@ -7,8 +7,9 @@ from django.urls import reverse_lazy, reverse
 from django.utils.translation import ugettext_lazy as _
 
 from django_registration.backends.activation import views as registration_views
+from rest_framework import viewsets, permissions
 
-from . import forms
+from . import forms, serializers, permissions as custom_permissions
 
 UserModel = get_user_model()
 
@@ -91,3 +92,43 @@ class UserDetailView(mixins.LoginRequiredMixin, generic.DetailView):
 
     def get_queryset(self):
         return super().get_queryset().filter(pk=self.request.user.pk)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserSerializer
+    http_method_names = ("get", "post", "put", "patch", "delete")
+
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+
+        if self.request.method in ("PUT", "PATCH"):
+            serializer_class = serializers.UserUpdateSerializer
+
+        return serializer_class
+
+    def get_authenticators(self):
+        if self.request.method == "POST":
+            return ()
+
+        return super().get_authenticators()
+
+    def get_permissions(self):
+        permission_classes = super().get_permissions()
+
+        if self.request.method == "POST":
+            permission_classes.append(custom_permissions.IsNotAuthenticated())
+        else:
+            permission_classes.append(permissions.IsAuthenticated())
+
+            # This is usually not needed because the queryset will prohibit
+            # editing other user's data but if the queryset is overrided for
+            # whaterver reason this check will also prevent the modification.
+            # The queryset will return 404 which is preferred while the lack
+            # of permissions will cause 403 revealing the existence of the
+            # user.
+            permission_classes.append(custom_permissions.IsHimself())
+
+        return permission_classes
+
+    def get_queryset(self):
+        return UserModel.objects.filter(pk=self.request.user.pk)
