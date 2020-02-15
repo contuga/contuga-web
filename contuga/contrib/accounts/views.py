7c7e4ea1bf6fffe21efa3c53856fa0d883e996de
Hristo Gatsinski <gatsinski@gmail.com>
2020-02-15 12:31:51 +0200

    Add Accounts API

--- contuga/contrib/accounts/views.py
+++ contuga/contrib/accounts/views.py
@@ -3,8 +3,10 @@ from django.views import generic
 from django.contrib.auth import mixins
 from django.urls import reverse_lazy
 
+from rest_framework import viewsets, permissions
+
 from contuga.mixins import OnlyOwnedByCurrentUserMixin
-from . import models
+from . import models, serializers
 
 
 class AccountCreateView(mixins.LoginRequiredMixin, generic.CreateView):
@@ -48,3 +50,19 @@ class AccountDeleteView(
 ):
     model = models.Account
     success_url = reverse_lazy("accounts:list")
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.AccountSerializer
+    http_method_names = ("get", "post", "put", "patch", "delete")
+
+    def get_permissions(self):
+        permission_classes = super().get_permissions()
+        permission_classes.append(permissions.IsAuthenticated())
+        return permission_classes
+
+    def get_queryset(self):
+        return models.Account.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
