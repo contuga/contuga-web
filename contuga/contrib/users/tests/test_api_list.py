478d7ef61d6628c879ea4a2ca448ad80e2c844e5
Hristo Gatsinski <gatsinski@gmail.com>
2020-02-15 12:29:41 +0200

    Add Users API

---
+++ contuga/contrib/users/tests/test_api_list.py
@@ -0,0 +1,68 @@
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+from rest_framework.test import APITestCase
+
+from ..models import User
+
+UserModel = get_user_model()
+
+
+class UserListTestCase(APITestCase):
+    def test_get(self):
+        url = reverse("user-list")
+
+        user = UserModel.objects.create_user("john.doe@example.com", "password")
+
+        # Creating another user to make sure the currently logged in user cannot see others
+        UserModel.objects.create_user("richard.roe@example.com", "password")
+
+        self.client.force_login(user)
+
+        response = self.client.get(url, format="json")
+
+        # Assert status code is correct
+        self.assertEqual(response.status_code, 200)
+
+        # Assert correct data is returned
+        expected_response = {
+            "count": 1,
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "url": response.wsgi_request.build_absolute_uri(
+                        reverse("user-detail", args=[user.pk])
+                    ),
+                    "email": user.email,
+                    "last_login": user.last_login.astimezone().isoformat(),
+                    "date_joined": user.date_joined.astimezone().isoformat(),
+                }
+            ],
+        }
+
+        self.assertDictEqual(response.json(), expected_response)
+
+    def test_post(self):
+        url = reverse("user-list")
+
+        data = {"email": "john.doe@example.com", "password": "password"}
+
+        response = self.client.post(url, data=data, format="json")
+
+        # Assert status code is correct
+        self.assertEqual(response.status_code, 201)
+
+        # Assert correct data is returned
+        user = User.objects.order_by("pk").last()
+
+        expected_response = {
+            "url": response.wsgi_request.build_absolute_uri(
+                reverse("user-detail", args=[user.pk])
+            ),
+            "email": user.email,
+            "last_login": None,
+            "date_joined": user.date_joined.astimezone().isoformat(),
+        }
+
+        self.assertDictEqual(response.json(), expected_response)
