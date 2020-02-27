d1edff2ce50c942a3b4dd4b780ceae28b1b066f3
Hristo Gatsinski <gatsinski@gmail.com>
2020-02-27 21:54:47 +0200

    Add Analytics API

--- contuga/contrib/analytics/views.py
+++ contuga/contrib/analytics/views.py
@@ -1,10 +1,17 @@
 import json
 
+from django import forms
+from django.core.exceptions import ValidationError
 from django.core.serializers.json import DjangoJSONEncoder
 from django.views.generic.base import TemplateView
 from django.contrib.auth import mixins
 
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+
 from . import utils
+from .serializers import MonthlyReportSerializer
+from .api_filters import MonthlyReportFilterBackend
 
 
 class AnalyticsView(mixins.LoginRequiredMixin, TemplateView):
@@ -17,3 +24,37 @@ class AnalyticsView(mixins.LoginRequiredMixin, TemplateView):
         context["reports"] = json.dumps(reports, cls=DjangoJSONEncoder)
 
         return context
+
+
+class AnalyticsViewSet(viewsets.ViewSet):
+    serlizer_class = MonthlyReportSerializer
+    filter_backends = (MonthlyReportFilterBackend,)
+
+    def get_permissions(self):
+        permission_classes = super().get_permissions()
+        permission_classes.append(permissions.IsAuthenticated())
+        return permission_classes
+
+    def list(self, request):
+        date_field = forms.DateField()
+        try:
+            start_date = date_field.clean(self.request.query_params.get("start_date"))
+        except ValidationError:
+            start_date = None
+
+        reports = utils.get_monthly_reports(
+            user=self.request.user, start_date=start_date
+        )
+
+        serializer = MonthlyReportSerializer(
+            instance=reports, many=True, context={"request": request}
+        )
+
+        return Response(
+            {
+                "count": len(reports),
+                "next": None,
+                "previous": None,
+                "results": serializer.data,
+            }
+        )
