76bf1ce81a8c90c7185c0618971def3ce0d92307
Hristo Gatsinski <gatsinski@gmail.com>
2020-03-29 12:28:08 +0300

    Replace all re_path urls with path

--- contuga/contrib/users/urls.py
+++ contuga/contrib/users/urls.py
@@ -1,61 +1,59 @@
-from django.urls import re_path
+from django.urls import path
 from django.views.generic import base as base_views
 
 from . import views
 
 
 urlpatterns = [
-    re_path("^login/$", views.LoginView.as_view(), name="login"),
-    re_path(r"^logout/$", views.LogoutView.as_view(), name="logout"),
-    re_path(r"^register/$", views.RegistrationView.as_view(), name="registration"),
-    re_path(
-        r"^registration/complete/$",
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("register/", views.RegistrationView.as_view(), name="registration"),
+    path(
+        "registration/complete/",
         base_views.TemplateView.as_view(
             template_name="users/registration_complete.html"
         ),
         name="registration_complete",
     ),
-    re_path(
-        r"^registration/disallowed/$",
+    path(
+        "registration/disallowed/",
         base_views.TemplateView.as_view(
             template_name="users/registration_disallowed.html"
         ),
         name="registration_disallowed",
     ),
-    re_path(
-        r"^activate/(?P<activation_key>[-:\w]+)/$",
+    path(
+        "activate/<str:activation_key>/",
         views.ActivationView.as_view(),
         name="activate",
     ),
-    re_path(
-        r"^activation/complete/$",
+    path(
+        "activation/complete/",
         base_views.TemplateView.as_view(template_name="users/activation_complete.html"),
         name="activation_complete",
     ),
-    re_path(r"^(?P<pk>\d+)/$", views.UserDetailView.as_view(), name="profile"),
-    re_path(
-        "^password_change/$", views.PasswordChangeView.as_view(), name="password_change"
+    path("<int:pk>/", views.UserDetailView.as_view(), name="profile"),
+    path(
+        "password_change/", views.PasswordChangeView.as_view(), name="password_change"
     ),
-    re_path(
-        "^password_change/done/$",
+    path(
+        "password_change/done/",
         views.PasswordChangeDoneView.as_view(),
         name="password_change_done",
     ),
-    re_path(
-        "password_reset/$", views.PasswordResetView.as_view(), name="password_reset"
-    ),
-    re_path(
-        "^password_reset/done/$",
+    path("password_reset/", views.PasswordResetView.as_view(), name="password_reset"),
+    path(
+        "password_reset/done/",
         views.PasswordResetDoneView.as_view(),
         name="password_reset_done",
     ),
-    re_path(
-        "^reset/(?P<uidb64>[0-9A-Za-z_-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
+    path(
+        "reset/<uidb64>/<token>/",
         views.PasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
     ),
-    re_path(
-        "^reset/done/$",
+    path(
+        "reset/done/",
         views.PasswordResetCompleteView.as_view(),
         name="password_reset_complete",
     ),
