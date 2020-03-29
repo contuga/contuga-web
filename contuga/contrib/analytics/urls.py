39a5a3a9cd62fc996b811ae710f7bc946066525f
Hristo Gatsinski <gatsinski@gmail.com>
2020-03-29 12:28:08 +0300

    Replace all re_path urls with path

--- contuga/contrib/analytics/urls.py
+++ contuga/contrib/analytics/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import re_path
+from django.urls import path
 from . import views
 
-urlpatterns = [re_path(r"^$", views.AnalyticsView.as_view(), name="list")]
+urlpatterns = [path("", views.AnalyticsView.as_view(), name="list")]
