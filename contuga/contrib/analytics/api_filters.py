be2adf029edef8545b44dc21ed374471394190bc
Hristo Gatsinski <gatsinski@gmail.com>
2020-04-04 15:41:05 +0300

    Add daily reports and refactor analytics

--- contuga/contrib/analytics/api_filters.py
+++ contuga/contrib/analytics/api_filters.py
@@ -3,10 +3,13 @@ import coreapi
 from rest_framework.filters import BaseFilterBackend
 
 
-class MonthlyReportFilterBackend(BaseFilterBackend):
+class ReportsFilterBackend(BaseFilterBackend):
     def get_schema_fields(self, view):
         return [
             coreapi.Field(
+                name="report_unit", location="query", required=False, type="string"
+            ),
+            coreapi.Field(
                 name="start_date", location="query", required=False, type="string"
-            )
+            ),
         ]
