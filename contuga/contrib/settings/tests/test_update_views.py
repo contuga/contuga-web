84d020e21d9876071247adb7c097d4067b9440ec
Hristo Gatsinski <gatsinski@gmail.com>
2020-04-12 15:52:56 +0300

    Add separate categories for incomes and expenditures

---
+++ contuga/contrib/settings/tests/test_update_views.py
@@ -0,0 +1,159 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import get_user_model
+
+from contuga.mixins import TestMixin
+from contuga.contrib.categories import constants as category_constants
+
+from ..models import Settings
+
+UserModel = get_user_model()
+
+
+class SettingsViewTests(TestCase, TestMixin):
+    def setUp(self):
+        self.user = UserModel.objects.create_user("john.doe@example.com", "password")
+        self.settings = Settings.objects.last()
+        self.client.force_login(self.user)
+
+    def test_update_get(self):
+        incomes_category = self.create_category(
+            transaction_type=category_constants.INCOME
+        )
+        expenditures_category = self.create_category(
+            transaction_type=category_constants.EXPENDITURE
+        )
+        account = self.create_account()
+
+        self.settings.default_incomes_category = incomes_category
+        self.settings.default_expenditures_category = expenditures_category
+        self.settings.default_account = account
+        self.settings.save()
+
+        url = reverse("settings:update")
+        response = self.client.get(url)
+
+        # Assert status code is correct
+        self.assertEqual(response.status_code, 200)
+
+        # Assert instance is correct
+        instance = response.context.get("object")
+        self.assertEqual(instance, self.settings)
+
+        # Assert initial form data is correct
+        form = response.context.get("form")
+        form_data = {
+            "default_incomes_category": form.initial["default_incomes_category"],
+            "default_expenditures_category": form.initial[
+                "default_expenditures_category"
+            ],
+            "default_account": form.initial["default_account"],
+        }
+        expected_data = {
+            "default_incomes_category": self.settings.default_incomes_category.pk,
+            "default_expenditures_category": self.settings.default_expenditures_category.pk,
+            "default_account": self.settings.default_account.pk,
+        }
+        self.assertDictEqual(form_data, expected_data)
+
+    def test_update(self):
+        incomes_category = self.create_category(
+            transaction_type=category_constants.INCOME
+        )
+        expenditures_category = self.create_category(
+            transaction_type=category_constants.EXPENDITURE
+        )
+        account = self.create_account()
+        data = {
+            "default_incomes_category": incomes_category.pk,
+            "default_expenditures_category": expenditures_category.pk,
+            "default_account": account.pk,
+        }
+
+        url = reverse("settings:update")
+        response = self.client.post(url, data=data, follow=True)
+
+        # Assert user is redirected to detail view
+        self.assertRedirects(
+            response,
+            reverse("settings:detail"),
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True,
+        )
+
+        # Assert settings are updated
+        updated_settings = Settings.objects.get(pk=self.settings.pk)
+        settings_data = {
+            "pk": updated_settings.pk,
+            "default_incomes_category": updated_settings.default_incomes_category,
+            "default_expenditures_category": updated_settings.default_expenditures_category,
+            "default_account": updated_settings.default_account,
+        }
+        expected_data = {
+            "pk": self.user.pk,
+            "default_incomes_category": incomes_category,
+            "default_expenditures_category": expenditures_category,
+            "default_account": account,
+        }
+        self.assertDictEqual(settings_data, expected_data)
+
+    def test_update_with_invalid_default_categories(self):
+        incomes_category = self.create_category(
+            transaction_type=category_constants.INCOME
+        )
+        expenditures_category = self.create_category(
+            transaction_type=category_constants.EXPENDITURE
+        )
+        account = self.create_account()
+
+        self.settings.default_incomes_category = incomes_category
+        self.settings.default_expenditures_category = expenditures_category
+        self.settings.default_account = account
+        self.settings.save()
+
+        data = {
+            "default_incomes_category": expenditures_category.pk,
+            "default_expenditures_category": incomes_category.pk,
+            "default_account": account.pk,
+        }
+
+        url = reverse("settings:update")
+        response = self.client.post(url, data=data, follow=True)
+
+        # Assert status code is correct
+        self.assertEqual(response.status_code, 200)
+
+        form = response.context["form"]
+
+        # Assert form is invalid
+        self.assertFalse(form.is_valid())
+
+        # Assert form errors are correct
+        expected_message = _(
+            "Select a valid choice. That choice is not one of the available choices."
+        )
+        self.assertEqual(
+            form.errors,
+            {
+                "default_incomes_category": [expected_message],
+                "default_expenditures_category": [expected_message],
+            },
+        )
+
+        # Assert settings are not updated
+        updated_settings = Settings.objects.get(pk=self.settings.pk)
+        settings_data = {
+            "pk": updated_settings.pk,
+            "default_incomes_category": updated_settings.default_incomes_category,
+            "default_expenditures_category": updated_settings.default_expenditures_category,
+            "default_account": updated_settings.default_account,
+        }
+        expected_data = {
+            "pk": self.settings.pk,
+            "default_incomes_category": self.settings.default_incomes_category,
+            "default_expenditures_category": self.settings.default_expenditures_category,
+            "default_account": self.settings.default_account,
+        }
+        self.assertDictEqual(settings_data, expected_data)
--- contuga/contrib/transactions/views.py
+++ contuga/contrib/transactions/views.py
@@ -1,4 +1,5 @@
 import json
+from collections import defaultdict
 
 from django import forms as django_forms
 from django.http import HttpResponseRedirect
@@ -12,6 +13,10 @@ from import_export.mixins import ExportViewFormMixin
 from rest_framework import viewsets, permissions
 
 from contuga.contrib.accounts import models as account_models
+from contuga.contrib.categories import (
+    constants as category_constants,
+    models as category_models,
+)
 from contuga.contrib.settings import models as settings_models
 from contuga.mixins import OnlyAuthoredByCurrentUserMixin
 from contuga import views
@@ -22,9 +27,20 @@ class BaseTransactionFormViewMixin:
     def get_form(self):
         form = super().get_form()
         category_field = form.fields["category"]
-        category_field.queryset = category_field.queryset.filter(
-            author=self.request.user
-        )
+
+        queryset = category_field.queryset.filter(author=self.request.user)
+
+        if self.request.POST and self.request.POST.get("type") == constants.INCOME:
+            category_field.queryset = queryset.filter(
+                transaction_type__in=(category_constants.ALL, category_constants.INCOME)
+            )
+        else:
+            category_field.queryset = queryset.filter(
+                transaction_type__in=[
+                    category_constants.ALL,
+                    category_constants.EXPENDITURE,
+                ]
+            )
 
         description_field = form.fields["description"]
         description_field.widget = django_forms.Textarea(attrs={"rows": 3})
@@ -33,6 +49,65 @@ class BaseTransactionFormViewMixin:
         account_field.queryset = account_field.queryset.filter(owner=self.request.user)
 
         return form
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        settings = (
+            settings_models.Settings.objects.filter(user=self.request.user)
+            .select_related(
+                "default_expenditures_category",
+                "default_incomes_category",
+                "default_account",
+            )
+            .first()
+        )
+        form = context["form"]
+        category_field = form.fields["category"]
+        category_field.initial = settings.default_expenditures_category
+
+        categories = category_models.Category.objects.filter(author=self.request.user)
+
+        grouped_categories = defaultdict(list)
+        for category in categories:
+            if category.transaction_type == category_constants.INCOME:
+                grouped_categories[constants.INCOME].append(
+                    {
+                        "id": category.pk,
+                        "name": category.name,
+                        "selected": category == settings.default_incomes_category,
+                    }
+                )
+            elif category.transaction_type == category_constants.EXPENDITURE:
+                grouped_categories[constants.EXPENDITURE].append(
+                    {
+                        "id": category.pk,
+                        "name": category.name,
+                        "selected": category == settings.default_expenditures_category,
+                    }
+                )
+            else:
+                grouped_categories[constants.INCOME].append(
+                    {
+                        "id": category.pk,
+                        "name": category.name,
+                        "selected": category == settings.default_incomes_category,
+                    }
+                )
+                grouped_categories[constants.EXPENDITURE].append(
+                    {
+                        "id": category.pk,
+                        "name": category.name,
+                        "selected": category == settings.default_expenditures_category,
+                    }
+                )
+
+        context["category_choices"] = json.dumps(grouped_categories)
+
+        account_field = form.fields["account"]
+        account_field.initial = settings.default_account
+
+        return context
 
 
 class TransactionCreateView(
@@ -43,18 +118,9 @@ class TransactionCreateView(
 
     def get_form(self):
         form = super().get_form()
-        settings = (
-            settings_models.Settings.objects.filter(user=self.request.user)
-            .select_related("default_category", "default_account")
-            .first()
-        )
-
-        category_field = form.fields["category"]
-        category_field.initial = settings.default_category
 
         account_field = form.fields["account"]
         account_field.queryset = account_field.queryset.filter(is_active=True)
-        account_field.initial = settings.default_account
 
         return form
 
@@ -216,6 +282,29 @@ class TransactionViewSet(OnlyAuthoredByCurrentUserMixin, viewsets.ModelViewSet):
 
     def get_queryset(self):
         return models.Transaction.objects.filter(author=self.request.user)
+
+    def get_serializer(self, *args, **kwargs):
+        serializer = super().get_serializer(*args, **kwargs)
+        if self.request.method in ("POST", "PUT", "PATCH"):
+            category_field = serializer.fields["category"]
+
+            queryset = category_field.queryset.filter(author=self.request.user)
+
+            if self.request.data.get("type") == constants.INCOME:
+                category_field.queryset = queryset.filter(
+                    transaction_type__in=[
+                        category_constants.ALL,
+                        category_constants.INCOME,
+                    ]
+                )
+            else:
+                category_field.queryset = queryset.filter(
+                    transaction_type__in=[
+                        category_constants.ALL,
+                        category_constants.EXPENDITURE,
+                    ]
+                )
+        return serializer
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
