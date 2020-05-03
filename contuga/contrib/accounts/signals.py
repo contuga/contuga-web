641d897f621ee3abd2aa7c06ef311c07e6559018
Hristo Gatsinski <gatsinski@gmail.com>
2020-05-03 20:53:01 +0300

    Add balance update on transaction post_delete signal

--- contuga/contrib/accounts/signals.py
+++ contuga/contrib/accounts/signals.py
@@ -1,11 +1,14 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from contuga.contrib.transactions.models import Transaction
 
 
-@receiver(post_save, sender=Transaction, dispatch_uid="update_account_balance")
-def update_account_balance(sender, instance, created, **kwargs):
+@receiver(post_save, sender=Transaction, dispatch_uid="update_account_balance_on_save")
+@receiver(
+    post_delete, sender=Transaction, dispatch_uid="update_account_balance_on_delete"
+)
+def update_account_balance(sender, instance, **kwargs):
     account = instance.account
     new_balance = account.calculate_balance()
 
