c4db4fb21a41c9ead12e96a2fc35a2e1972d41e1
Hristo Gatsinski <gatsinski@gmail.com>
2020-05-07 23:26:58 +0300

    Refactor transaction views

--- contuga/contrib/transactions/views.py
+++ contuga/contrib/transactions/views.py
@@ -1,7 +1,5 @@
 import json
-from collections import defaultdict
 
-from django import forms as django_forms
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.views import generic
@@ -13,124 +11,19 @@ from import_export.mixins import ExportViewFormMixin
 from rest_framework import viewsets, permissions
 
 from contuga.contrib.accounts import models as account_models
-from contuga.contrib.categories import (
-    constants as category_constants,
-    models as category_models,
-)
+from contuga.contrib.categories import constants as category_constants
 from contuga.contrib.settings import models as settings_models
 from contuga.mixins import OnlyAuthoredByCurrentUserMixin
 from contuga import views
+from .mixins import BaseTransactionFormViewMixin, GroupedCategoriesMixin
 from . import models, filters, resources, forms, constants, serializers
 
 
-class BaseTransactionFormViewMixin:
-    def get_form(self):
-        form = super().get_form()
-        category_field = form.fields["category"]
-
-        queryset = category_field.queryset.filter(author=self.request.user)
-
-        if self.request.POST:
-            if self.request.POST.get("type") == constants.INCOME:
-                category_field.queryset = queryset.filter(
-                    transaction_type__in=(
-                        category_constants.ALL,
-                        category_constants.INCOME,
-                    )
-                )
-            else:
-                category_field.queryset = queryset.filter(
-                    transaction_type__in=(
-                        category_constants.ALL,
-                        category_constants.EXPENDITURE,
-                    )
-                )
-        else:
-            if form.instance.type == constants.INCOME:
-                category_field.queryset = queryset.filter(
-                    transaction_type__in=(
-                        category_constants.ALL,
-                        category_constants.INCOME,
-                    )
-                )
-            else:
-                category_field.queryset = queryset.filter(
-                    transaction_type__in=[
-                        category_constants.ALL,
-                        category_constants.EXPENDITURE,
-                    ]
-                )
-
-        description_field = form.fields["description"]
-        description_field.widget = django_forms.Textarea(attrs={"rows": 3})
-
-        account_field = form.fields["account"]
-        account_field.queryset = account_field.queryset.filter(owner=self.request.user)
-
-        return form
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        settings = (
-            settings_models.Settings.objects.filter(user=self.request.user)
-            .select_related(
-                "default_expenditures_category",
-                "default_incomes_category",
-                "default_account",
-            )
-            .first()
-        )
-        form = context["form"]
-        category_field = form.fields["category"]
-        category_field.initial = settings.default_expenditures_category
-
-        categories = category_models.Category.objects.filter(author=self.request.user)
-
-        grouped_categories = defaultdict(list)
-        for category in categories:
-            if category.transaction_type == category_constants.INCOME:
-                grouped_categories[constants.INCOME].append(
-                    {
-                        "id": category.pk,
-                        "name": category.name,
-                        "selected": category == settings.default_incomes_category,
-                    }
-                )
-            elif category.transaction_type == category_constants.EXPENDITURE:
-                grouped_categories[constants.EXPENDITURE].append(
-                    {
-                        "id": category.pk,
-                        "name": category.name,
-                        "selected": category == settings.default_expenditures_category,
-                    }
-                )
-            else:
-                grouped_categories[constants.INCOME].append(
-                    {
-                        "id": category.pk,
-                        "name": category.name,
-                        "selected": category == settings.default_incomes_category,
-                    }
-                )
-                grouped_categories[constants.EXPENDITURE].append(
-                    {
-                        "id": category.pk,
-                        "name": category.name,
-                        "selected": category == settings.default_expenditures_category,
-                    }
-                )
-
-        context["category_choices"] = json.dumps(grouped_categories)
-
-        account_field = form.fields["account"]
-        account_field.initial = settings.default_account
-
-        return context
-
-
 class TransactionCreateView(
-    BaseTransactionFormViewMixin, mixins.LoginRequiredMixin, generic.CreateView
+    BaseTransactionFormViewMixin,
+    GroupedCategoriesMixin,
+    mixins.LoginRequiredMixin,
+    generic.CreateView,
 ):
     model = models.Transaction
     fields = ("type", "amount", "account", "category", "description")
@@ -150,6 +43,7 @@ class TransactionCreateView(
 
 
 class TransactionListView(
+    GroupedCategoriesMixin,
     OnlyAuthoredByCurrentUserMixin,
     mixins.LoginRequiredMixin,
     views.FilteredListView,
@@ -178,53 +72,17 @@ class TransactionListView(
             )
             .first()
         )
+        context["category_choices"] = json.dumps(self.get_category_choices(settings))
+        self.apply_initial_values(form, settings)
 
-        account_field = form.fields["account"]
-        account_field.initial = settings.default_account
+        return context
 
+    def apply_initial_values(self, form, settings):
         category_field = form.fields["category"]
         category_field.initial = settings.default_expenditures_category
 
-        categories = category_models.Category.objects.filter(author=self.request.user)
-
-        grouped_categories = defaultdict(list)
-
-        for category in categories:
-            if category.transaction_type == category_constants.INCOME:
-                grouped_categories[constants.INCOME].append(
-                    {
-                        "id": category.pk,
-                        "name": category.name,
-                        "selected": category == settings.default_incomes_category,
-                    }
-                )
-            elif category.transaction_type == category_constants.EXPENDITURE:
-                grouped_categories[constants.EXPENDITURE].append(
-                    {
-                        "id": category.pk,
-                        "name": category.name,
-                        "selected": category == settings.default_expenditures_category,
-                    }
-                )
-            else:
-                grouped_categories[constants.INCOME].append(
-                    {
-                        "id": category.pk,
-                        "name": category.name,
-                        "selected": category == settings.default_incomes_category,
-                    }
-                )
-                grouped_categories[constants.EXPENDITURE].append(
-                    {
-                        "id": category.pk,
-                        "name": category.name,
-                        "selected": category == settings.default_expenditures_category,
-                    }
-                )
-
-        context["category_choices"] = json.dumps(grouped_categories)
-
-        return context
+        account_field = form.fields["account"]
+        account_field.initial = settings.default_account
 
 
 class TransactionDetailView(
@@ -235,6 +93,7 @@ class TransactionDetailView(
 
 class TransactionUpdateView(
     BaseTransactionFormViewMixin,
+    GroupedCategoriesMixin,
     OnlyAuthoredByCurrentUserMixin,
     mixins.LoginRequiredMixin,
     generic.UpdateView,
