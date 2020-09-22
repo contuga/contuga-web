c24aa9cf5ae1a01dbc85a6454283da25113ed656
Hristo Gatsinski <gatsinski@gmail.com>
2020-09-22 14:33:26 +0300

    Use UUID for currencies

--- contuga/contrib/currencies/models.py
+++ contuga/contrib/currencies/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
@@ -9,6 +11,7 @@ UserModel = get_user_model()
 
 
 class Currency(TimestampModel):
+    uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
     name = models.CharField(_("Name"), max_length=254)
     code = models.CharField(_("ISO code"), max_length=3, blank=True)
     nominal = models.PositiveSmallIntegerField(_("Nominal"), default=1)
