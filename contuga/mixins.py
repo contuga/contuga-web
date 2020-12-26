a2490339fd11ec53117817ec6b98723bf609d07d
Hristo Gatsinski <gatsinski@gmail.com>
2020-12-26 14:12:49 +0200

    Allow passing of extra arguments to create_user test mixin

--- contuga/mixins.py
+++ contuga/mixins.py
@@ -21,8 +21,12 @@ class OnlyOwnedByCurrentUserMixin:
 
 
 class TestMixin:
-    def create_user(self, email="john.doe@example.com", password="password"):
-        return UserModel.objects.create_user(email=email, password=password)
+    def create_user(
+        self, email="john.doe@example.com", password="password", **extra_fields
+    ):
+        return UserModel.objects.create_user(
+            email=email, password=password, **extra_fields
+        )
 
     def create_category(
         self,
