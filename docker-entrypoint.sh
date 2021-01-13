ebf3bad4682c6e1d5ff228daea69357963cad28b
Hristo Gatsinski <gatsinski@gmail.com>
2021-01-14 00:41:46 +0200

    Add docker-compose setup for local test of production environment

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Collecting static files"
+python3 manage.py collectstatic --noinput
+
+echo "Applying database migrations"
+python3 manage.py migrate
+
+echo "Compiling translations"
+python3 ./manage.py compilemessages
+
+echo "Starting server"
+gunicorn contuga.wsgi:application -w 2 -b :8000
+# uwsgi --http :8000 --module contuga.wsgi
