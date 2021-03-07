ad59188b1b1310ed1e318d73e83b42148e61b70e
Hristo Gatsinski <gatsinski@gmail.com>
2021-03-07 23:50:57 +0200

    Add Rollbar integration

--- contuga/settings/environments/development.py
+++ contuga/settings/environments/development.py
@@ -1,3 +1,5 @@
+from .components.base import ROLLBAR
+
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
@@ -5,3 +7,5 @@ STATICFILES_STORAGE = "pipeline.storage.NonPackagingPipelineStorage"
 SECRET_KEY = "SECRET_KEY"
 RECAPTCHA_PRIVATE_KEY = "RECAPTCHA_PRIVATE_KEY"
 RECAPTCHA_PUBLIC_KEY = "RECAPTCHA_PUBLIC_KEY"
+
+ROLLBAR["environment"] = "development"
