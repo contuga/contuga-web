331cc74950264179dc51b163c0539bace9a8dcd0
Hristo Gatsinski <gatsinski@gmail.com>
2022-11-25 21:42:54 +0200

    Collect only needed directories from node_modules

--- contuga/settings/components/static.py
+++ contuga/settings/components/static.py
@@ -11,7 +11,23 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "node_modules"),)
+STATICFILES_DIRS = (
+    ("vendor/bootstrap/scss", os.path.join(BASE_DIR, "node_modules/bootstrap/scss/")),
+    ("vendor/bootstrap/dist", os.path.join(BASE_DIR, "node_modules/bootstrap/dist/")),
+    (
+        "vendor/font-awesome/scss",
+        os.path.join(BASE_DIR, "node_modules/font-awesome/scss/"),
+    ),
+    (
+        "vendor/font-awesome/fonts",
+        os.path.join(BASE_DIR, "node_modules/font-awesome/fonts/"),
+    ),
+    ("vendor/daterangepicker", os.path.join(BASE_DIR, "node_modules/daterangepicker/")),
+    ("vendor/tagify/src", os.path.join(BASE_DIR, "node_modules/@yaireo/tagify/src/")),
+    ("vendor/tagify/dist", os.path.join(BASE_DIR, "node_modules/@yaireo/tagify/dist/")),
+    ("vendor/jquery/dist", os.path.join(BASE_DIR, "node_modules/jquery/dist/")),
+    ("vendor/chart.js/dist", os.path.join(BASE_DIR, "node_modules/chart.js/dist/")),
+)
 
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
@@ -28,10 +44,10 @@ PIPELINE = {
         "main": {
             "source_filenames": (
                 "scss/_variables.scss",
-                "bootstrap/scss/bootstrap.scss",
-                "font-awesome/scss/font-awesome.scss",
-                "daterangepicker/daterangepicker.css",
-                "@yaireo/tagify/src/tagify.scss",
+                "vendor/bootstrap/scss/bootstrap.scss",
+                "vendor/font-awesome/scss/font-awesome.scss",
+                "vendor/daterangepicker/daterangepicker.css",
+                "vendor/tagify/src/tagify.scss",
                 "scss/main.scss",
             ),
             "output_filename": "css/main.css",
@@ -40,12 +56,12 @@ PIPELINE = {
     "JAVASCRIPT": {
         "main": {
             "source_filenames": (
-                "jquery/dist/jquery.js",
-                "bootstrap/dist/js/bootstrap.bundle.js",
-                "chart.js/dist/Chart.bundle.js",
-                "daterangepicker/moment.min.js",
-                "daterangepicker/daterangepicker.js",
-                "@yaireo/tagify/dist/tagify.min.js",
+                "vendor/jquery/dist/jquery.js",
+                "vendor/bootstrap/dist/js/bootstrap.bundle.js",
+                "vendor/chart.js/dist/Chart.bundle.js",
+                "vendor/daterangepicker/moment.min.js",
+                "vendor/daterangepicker/daterangepicker.js",
+                "vendor/tagify/dist/tagify.min.js",
                 "js/main.js",
             ),
             "output_filename": "js/main.js",
